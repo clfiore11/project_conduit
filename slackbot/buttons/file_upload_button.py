32e38c51d1c95dea3fad2369e5ba542901d48d18
Sylvester-Ikpa <sylvester.ikpa@doordash.com>
2022-12-03 16:46:27 -0800

    comments some of the  my code and functions

--- slackbot/buttons/file_upload_button.py
+++ slackbot/buttons/file_upload_button.py
@@ -17,6 +17,12 @@ class FileUploadButton:
         self.action_id = f"{self.name}_button_clicked"
 
     def __dict__(self):
+        """"
+        __dict__ provided a slack button framework
+
+        return: a button with the elements determined by the user
+
+        """
         return (
             {
                 "type": "button",
@@ -26,14 +32,23 @@ class FileUploadButton:
         )
 
     def upload_file(self, channel, manager):
+        """
+        upload_file runs the the gdrive_update_upload and generate_pdf fucntions to genereate and store the reports requested.
 
+        channel: the slack channel / dm that the data should be uploaded to.
+        manager: preconfigured value for the report that a user would like to request. For demostration purposes, we used manager.
+
+        return: No explicit return statement or value.
+
+        """
+        
+        # storing current directory
         owd = os.getcwd()
 
         # change dir to function dir
         os.chdir("utils/tableau_api")
         
-        # generate pdf
-        #TODO: Remember to change these args
+        # user can configure this to be any dasboard or tab in tableau
         generate_pdf('[Draft] NPS One Pagers Mock up', 'NPSOnePagerDIRECTROLLUP', 'Manager', [self.manager])
        
         # change dir to function dir
@@ -44,4 +59,5 @@ class FileUploadButton:
 
         self.app.client.files_upload(channels=channel, file=self.file_path)
 
+        # changing directory to the original state
         os.chdir(owd)
